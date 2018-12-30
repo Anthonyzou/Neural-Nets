0c5e2fd96b5795bc09eb0caec0a926e3488dc8fe
Anthony <ou.anthony@gmail.com>
2018-12-30 15:55:45 -0700

    Add save and load function, add hidden layers

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const colors = require('./colors.json');
-const { NeuralNetwork, layer } = require('brain.js');
+const { NeuralNetwork } = require('brain.js');
 const chalk = require('chalk');
+const fs = require('fs');
 const _ = require('lodash');
 
 const vorpal = require('vorpal')();
 
 const net = new NeuralNetwork({
   inputLayer: { width: 3 },
-  hiddenLayers: [5],
+  hiddenLayers: [4, 2],
 });
+
 
 vorpal.command('rgb [rgb...]').action((args, cb) => {
   const [r, g, b] = args.rgb;
-  console.log(chalk.rgb(r, g, b)('#####'));
+  console.log(chalk.rgb(r, g, b).bgKeyword('white')('#####'));
   console.log(net.run(args.rgb));
   cb();
 });
-vorpal.command('train').action((args, cb) => {
-  const trainingData = colors.map(({ b, g, r, label }) => {
-    return {
-      input: [r / 255, g / 255, b / 255],
-      output: { [label]: 1 },
-    };
-  });
 
-  net.train(_.sampleSize(trainingData, 3000));
+vorpal.command('train').action((args, cb) => {
+  const trainingData = colors.map(({ b, g, r, label }) => ({
+    input: [r / 255, g / 255, b / 255],
+    output: { [label]: 1 },
+  }));
+
+  net.train(_.sampleSize(trainingData, 1000));
   cb();
 });
+
+vorpal.command('save').action((args, cb) => {
+  fs.writeFile('./save.json', JSON.stringify(net.toJSON()), cb);
+});
+
+vorpal.command('load').action((args, cb) => {
+  net.fromJSON(require('./save.json'));
+  cb();
+});
+
 vorpal.show('>');
